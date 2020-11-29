b55d9b173b9f8270cb10ec765ee4e5d99e0d1a46
Eoan O Dea <odeaeoan@gmail.com>
2020-11-29 18:31:30 +0100

    Updated class that is extended by SKin_ILC_Cards

--- skins/skin-ilc-cards.php
+++ skins/skin-ilc-cards.php
@@ -1,18 +1,18 @@
 <?php
 namespace ILC\Skins;
 
-use Elementor\Skin_Base;
-use Elementor\Controls_Manager;
-use Elementor\Core\Kits\Documents\Tabs\Global_Colors;
-use Elementor\Core\Kits\Documents\Tabs\Global_Typography;
-use Elementor\Group_Control_Image_Size;
-use Elementor\Group_Control_Typography;
-use Elementor\Widget_Base;
+use ElementorPro\Modules\Posts\Skins\Skin_Cards;
+// use Elementor\Controls_Manager;
+// use Elementor\Core\Kits\Documents\Tabs\Global_Colors;
+// use Elementor\Core\Kits\Documents\Tabs\Global_Typography;
+// use Elementor\Group_Control_Image_Size;
+// use Elementor\Group_Control_Typography;
+// use Elementor\Widget_Base;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-class Skin_ILC_Cards extends Skin_Base {
+class Skin_ILC_Cards extends Skin_Cards {
     
 }
