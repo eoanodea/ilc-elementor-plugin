bf918034006528c2f1bcea2b6fcddbf0f8f8c65d
Eoan O Dea <odeaeoan@gmail.com>
2021-01-06 16:24:15 +0000

    Bug fixing ilc posts

--- widgets/ilc-post-list.php
+++ widgets/ilc-post-list.php
@@ -16,7 +16,7 @@ use ElementorPro\Modules\QueryControl\Controls\Group_Control_Related;
 
 
 use ElementorPro\Modules\Posts\Skins;
-use ElementorPro\Modules\Posts\Widgets\Posts;
+use ElementorPro\Modules\Posts\Widgets\Posts_Base;
 // require_once(__DIR__ .'../skins/skin-ilc-cards.php');
 use ILC\Skins\Skin_ILC_Cards;
 
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Class Posts
  */
-class ILC_Posts extends Posts {
+class ILC_Posts extends Posts_Base {
 
 	public function get_name() {
 		return 'ilc_posts';
@@ -38,18 +38,6 @@ class ILC_Posts extends Posts {
 		return __( 'ILC Posts', 'elementor-pro' );
 	}
 
-	// public function get_keywords() {
-	// 	return [ 'posts', 'cpt', 'item', 'loop', 'query', 'cards', 'custom post type' ];
-	// }
-
-	// public function on_import( $element ) {
-	// 	if ( ! get_post_type_object( $element['settings']['posts_post_type'] ) ) {
-	// 		$element['settings']['posts_post_type'] = 'post';
-	// 	}
-
-	// 	return $element; 
-	// }
-
 	// protected function _register_skins() {
 	// 	$this->add_skin( new Skins\Skin_Cards( $this ) );
 	// 	$this->add_skin( new Skins\Skin_Classic( $this ) );
@@ -57,12 +45,64 @@ class ILC_Posts extends Posts {
 	// 	// $this->add_skin( new Skins\Skin_Full_Content( $this ) );
 	// }
 
-	// protected function _register_controls() {
-	// 	parent::_register_controls();
+	public function get_keywords() {
+		return [ 'posts', 'cpt', 'item', 'loop', 'query', 'cards', 'custom post type' ];
+	}
 
-	// 	$this->register_query_section_controls();
-	// 	$this->register_pagination_section_controls();
+	public function on_import( $element ) {
+		if ( ! get_post_type_object( $element['settings']['posts_post_type'] ) ) {
+			$element['settings']['posts_post_type'] = 'post';
+		}
 
-	// }
+		return $element;
+	}
+
+	protected function _register_skins() {
+		$this->add_skin( new Skins\Skin_Classic( $this ) );
+		$this->add_skin( new Skins\Skin_Cards( $this ) );
+		$this->add_skin( new Skins\Skin_Full_Content( $this ) );
+	}
+
+	protected function _register_controls() {
+		parent::_register_controls();
+
+		$this->register_query_section_controls();
+		$this->register_pagination_section_controls();
+	}
+
+	public function query_posts() {
+
+		$query_args = [
+			'posts_per_page' => $this->get_current_skin()->get_instance_value( 'posts_per_page' ),
+			'paged' => $this->get_current_page(),
+		];
+
+		/** @var Module_Query $elementor_query */
+		$elementor_query = Module_Query::instance();
+		$this->query = $elementor_query->get_query( $this, 'posts', $query_args, [] );
+	}
+
+	protected function register_query_section_controls() {
+		$this->start_controls_section(
+			'section_query',
+			[
+				'label' => __( 'Query', 'elementor-pro' ),
+				'tab' => Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_group_control(
+			Group_Control_Related::get_type(),
+			[
+				'name' => $this->get_name(),
+				'presets' => [ 'full' ],
+				'exclude' => [
+					'posts_per_page', //use the one from Layout section
+				],
+			]
+		);
+
+		$this->end_controls_section();
+	}
 
 }
