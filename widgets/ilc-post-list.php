1f122ab8d87fc50a2775061108ffb01ed1790dc9
Eoan O Dea <odeaeoan@gmail.com>
2021-01-06 16:14:37 +0000

    Bug fixing ilc posts

--- widgets/ilc-post-list.php
+++ widgets/ilc-post-list.php
@@ -46,8 +46,9 @@ class ILC_Posts extends Posts {
 	// }
 
 	protected function _register_skins() {
+		$this->add_skin( new Skins\Skin_Cards( $this ) );
 		$this->add_skin( new Skins\Skin_Classic( $this ) );
-		$this->add_skin( new Skin_ILC_Cards( $this ) );
+		// $this->add_skin( new Skin_ILC_Cards( $this ) );
 		// $this->add_skin( new Skins\Skin_Full_Content( $this ) );
 	}
 
@@ -59,39 +60,39 @@ class ILC_Posts extends Posts {
 
 	}
 
-	// public function query_posts() {
+	public function query_posts() {
 
-	// 	$query_args = [
-	// 		'posts_per_page' => $this->get_current_skin()->get_instance_value( 'posts_per_page' ),
-	// 		'paged' => $this->get_current_page(),
-	// 	];
+		$query_args = [
+			'posts_per_page' => $this->get_current_skin()->get_instance_value( 'posts_per_page' ),
+			'paged' => $this->get_current_page(),
+		];
 
-	// 	/** @var Module_Query $elementor_query */
-	// 	$elementor_query = Module_Query::instance();
-	// 	$this->query = $elementor_query->get_query( $this, 'posts', $query_args, [] );
-	// }
+		/** @var Module_Query $elementor_query */
+		$elementor_query = Module_Query::instance();
+		$this->query = $elementor_query->get_query( $this, 'posts', $query_args, [] );
+	}
 
-	// protected function register_query_section_controls() {
-	// 	$this->start_controls_section(
-	// 		'section_query',
-	// 		[
-	// 			'label' => __( 'Query', 'elementor-pro' ),
-	// 			'tab' => Controls_Manager::TAB_CONTENT,
-	// 		]
-	// 	);
+	protected function register_query_section_controls() {
+		$this->start_controls_section(
+			'section_query',
+			[
+				'label' => __( 'Query', 'elementor-pro' ),
+				'tab' => Controls_Manager::TAB_CONTENT,
+			]
+		);
 
-	// 	$this->add_group_control(
-	// 		Group_Control_Related::get_type(),
-	// 		[
-	// 			'name' => $this->get_name(),
-	// 			'presets' => [ 'full' ],
-	// 			'exclude' => [
-	// 				'posts_per_page', //use the one from Layout section
-	// 			],
-	// 		]
-	// 	);
+		$this->add_group_control(
+			Group_Control_Related::get_type(),
+			[
+				'name' => $this->get_name(),
+				'presets' => [ 'full' ],
+				'exclude' => [
+					'posts_per_page', //use the one from Layout section
+				],
+			]
+		);
 
-	// 	$this->end_controls_section();
-	// }
+		$this->end_controls_section();
+	}
 
 }
