6b74b4e6d6e7a09c66380e81d19592ef572cb375
Eoan O Dea <odeaeoan@gmail.com>
2021-01-18 18:33:32 +0000

    adding bookmark to posts in render post

--- plugin.php
+++ plugin.php
@@ -179,7 +179,7 @@ class Plugin {
 	public function __construct() {
 
 		// Register widget scripts
-		// add_action( 'elementor/frontend/after_register_scripts', [ $this, 'widget_scripts' ] );
+		add_action( 'elementor/frontend/after_register_scripts', [ $this, 'widget_scripts' ] );
 
 		// Register widgets
 		add_action( 'elementor/widgets/widgets_registered', [ $this, 'register_widgets' ] );
@@ -193,7 +193,7 @@ class Plugin {
 		add_action('wp_ajax_is_user_logged_in', 'ajax_check_user_logged_in');
 		add_action('wp_ajax_nopriv_is_user_logged_in', 'ajax_check_user_logged_in');
 
-		add_action('wp_footer', 'widget_scripts');
+		// add_action('wp_footer', 'widget_scripts');
 
 	}
 }
