ffcedaf0735b727b481c11ac98772127b5fd2cbd
Eoan O Dea <odeaeoan@gmail.com>
2021-01-18 17:49:41 +0000

    Modified action url

--- skins/skin-ilc-cards.php
+++ skins/skin-ilc-cards.php
@@ -24,10 +24,29 @@ class Skin_ILC_Cards extends Skin_Cards {
 		return __( 'ILC Cards', 'elementor-pro' );
 	}
 
-	// public function register_controls( Widget_Base $widget ) {
-	// 	parent::register_controls($widget);
+	public function register_controls( Widget_Base $widget ) {
+		// parent::register_controls($widget);
+
+		$this->parent = $widget;
+
+		$this->register_columns_controls();
+		$this->register_post_count_control();
+		$this->register_thumbnail_controls();
+		$this->register_title_controls();
+		$this->register_excerpt_controls();
+		$this->register_meta_data_controls();
+		$this->register_read_more_controls();
+		$this->register_badge_controls();
+		$this->register_avatar_controls();
 		
-	// }
+	}
+
+	public function register_design_controls() { // from parent
+		$this->register_design_layout_controls();
+		$this->register_design_card_controls();
+		$this->register_design_image_controls();
+		$this->register_design_content_controls();
+	}
 	// public function register_skin_controls( Widget_Base $widget ) {
 	// 	// parent::register_controls($widget);
 	// 	$this->parent = $widget;
@@ -47,7 +66,10 @@ class Skin_ILC_Cards extends Skin_Cards {
 	protected function _register_controls_actions() {
 		parent::_register_controls_actions();
 
-		add_action( 'elementor/element/ilc_posts/section_layout/before_section_end', [ $this, 'register_skin_controls' ] );
+		// From Skinss_Base
+		add_action('elementor/element/ilc_posts/section_layout/before_section_end', [$this, 'register_controls']);
+        add_action('elementor/element/ilc_posts/section_query/after_section_end', [$this, 'register_style_sections']);
+
 		add_action( 'elementor/element/ilc_posts/cards_section_design_image/before_section_end', [ $this, 'register_additional_design_image_controls' ] );
 
 	}
