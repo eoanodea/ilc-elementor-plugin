a8fd8c498cdfc6dc458acaa4d331221f4daaac4c
Eoan O Dea <odeaeoan@gmail.com>
2021-01-19 16:39:56 +0000

    added console log to js

--- plugin.php
+++ plugin.php
@@ -47,7 +47,7 @@ class Plugin {
 	 * @access public
 	 */
 	public function widget_scripts() {
-		wp_enqueue_style( 'ilc-elementor-widgets', plugins_url( '/assets/js/ilc-bkmk.js', __FILE__ ), [ 'jquery' ], false, true );
+		wp_register_script( 'ilc-elementor-widgets', plugins_url( '/assets/js/ilc-bkmk.js', __FILE__ ), [ 'jquery' ], false, true );
 
 
 
@@ -160,7 +160,7 @@ class Plugin {
 	public function __construct() {
 
 		// Register widget scripts
-		// add_action( 'elementor/frontend/after_register_scripts', [ $this, 'widget_scripts' ] );
+		add_action( 'elementor/frontend/after_register_scripts', [ $this, 'widget_scripts' ] );
 
 		// Register widgets
 		add_action( 'elementor/widgets/widgets_registered', [ $this, 'register_widgets' ] );
@@ -174,9 +174,6 @@ class Plugin {
 		add_action('wp_ajax_is_user_logged_in', 'ajax_check_user_logged_in');
 		add_action('wp_ajax_nopriv_is_user_logged_in', 'ajax_check_user_logged_in');
 
-		add_action('wp_enqueue_scripts', 'widget_scripts');
-
-		// add_action('wp_footer', 'widget_scripts');
 
 	}
 }
